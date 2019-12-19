812f25a8a49267262a51ad299d7ef4166af27632
Girin Ilya <39518953+Choos37ricK@users.noreply.github.com>
2019-12-19 20:20:50 +0300

    next step

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,16 @@
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 import org.bson.BsonDocument;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         MongoClient mongoClient = new MongoClient("127.0.0.1", 27017);
 
         MongoDatabase mongoDatabase = mongoClient.getDatabase("test");
@@ -16,15 +18,17 @@ public class Main {
         MongoCollection<Document> goodsCollection = mongoDatabase.getCollection("goods");
         MongoCollection<Document> shopsCollection = mongoDatabase.getCollection("shops");
 
-        goodsCollection.drop();
-        shopsCollection.drop();
+        //goodsCollection.drop();
+        //shopsCollection.drop();
+        Thread.sleep(1000);
+
+        Scanner scanner = new Scanner(System.in);
 
         while(true) {
             printCommands();
             System.out.print(">>> ");
-            Scanner scanner = new Scanner(System.in);
+            String commandLine = scanner.nextLine();
 
-            String commandLine = scanner.toString();
             if (commandLine.isEmpty()) {
                 System.out.println("Введите команду!");
                 continue;
@@ -32,39 +36,41 @@ public class Main {
 
             String[] commandParts = commandLine.split("\\s+");
             String command = commandParts[0].toLowerCase();
-            if (commandParts.length != 3) {
-                if (!command.equals("статистика_товаров")) {
-                    System.out.println("\nНеверный формат команды!\n");
-                    continue;
-                }
-            }
 
             switch (command) {
                 case "статистика_товаров":
+                    System.out.println("Общее количество товаров: " + goodsCollection.countDocuments());
+                    System.out.println("Средняя цена товара: " + goodsCollection.;
                     break;
                 case "добавить_магазин":
                     Document shop = new Document().append("name", commandParts[1]);
                     shopsCollection.insertOne(shop);
+                    System.out.println("Магазин добавлен:\n" + shopsCollection.find(shop).iterator().next().toJson());
                     break;
                 case "добавить_товар":
-                    Document goods = new Document()
+                    Document product = new Document()
                             .append("name", commandParts[1])
                             .append("price", commandParts[2]);
-                    goodsCollection.insertOne(goods);
+                    goodsCollection.insertOne(product);
+                    System.out.println("Товар добавлен:\n" + goodsCollection.find(product).iterator().next().toJson());
                     break;
                 case "выставить_товар":
-                    goodsCollection.find(); //нужно же сперва из коллекции товаров получить товар с его названием и ценой?
-                    //и затем вставить его в список товаров магазина?
-                    //но find() возвращает Iterable, а мне нужен один объект для вставки в запрос ниже
-                    //и да, разве запрос на добавление в список товаров будет настолько сложен и запутан? в консоли это прилично выглядит, а тут...
-                    shopsCollection.updateOne(Filters.eq("name", commandParts[2], new Document("$push", new Document("goods", commandParts[1]}}});
+                    Document product2Shop = goodsCollection.find(new BasicDBObject("name", commandParts[1])).iterator().next();
+                    shopsCollection.updateOne(
+                            Filters.eq("name", commandParts[2]),
+                            new Document("$push", new Document("goods", product2Shop)));
+                    System.out.println("Товар выставлен:\n" + shopsCollection.find(Filters.eq("name", commandParts[2])).iterator().next().toJson());
                     break;
-
+                default:
+                    System.out.println("\nНеверный формат команды!");
+                    break;
             }
         }
+
     }
 
     private static void printCommands() {
+        System.out.println();
         System.out.println("1. Чтобы добавить магазин введите: добавить_магазин Название");
         System.out.println("2. Чтобы добавить магазин введите: добавить_товар Название Количество");
         System.out.println("3. Чтобы добавить товар в магазин введите: выставить_товар НазваниеТовара НазваниеМагазина");
